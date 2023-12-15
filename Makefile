451bf7c690277435eb6ba71b7644a7751cb9ada6
Michał <m12@onet.com.pl>
2023-12-15 23:58:36 +0100

    Bugfixes
    Fixed automatic target alignment
    
    simplified rectangle rendering
    
    separated rendering from main into update()

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 all:
-	gcc -Wall main.c -o brekout `sdl2-config --cflags` `sdl2-config --libs` -lSDL2_image -lSDL2_ttf
+	gcc -Wall -Wextra main.c -o brekout `sdl2-config --cflags` `sdl2-config --libs` -lSDL2_image -lSDL2_ttf
 	./brekout
--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #define FPS 60
 #define BALL_SPEED 200
 #define PAD_SPEED 5
-#define TARGET_CAP 20
+#define TARGET_CAP 33
 #define TARGET_WIDTH 50
 // I'm pretty sure that this is not how delta time works
 #define DELTA_TIME 1.0/FPS
@@ -29,6 +29,7 @@ float pad_y = SCREEN_HEIGHT - 100;
 int ball_dx = 1;
 int ball_dy = 1;
 int score = 0;
+bool isPaused = false;
 
 typedef struct Target {
   SDL_Rect rect;
@@ -45,9 +46,9 @@ void InitTargets() {
     SDL_Rect* rect = &targets[i].rect;
     rect->x = (TARGET_WIDTH + 10)*ii;
     rect->y = (40)*row;
-    if (rect->x > SCREEN_WIDTH) {
+    if ((rect->x + TARGET_WIDTH) > SCREEN_WIDTH) {
       row++;
-      ii = 0;
+      ii = -1;
     }
     ii++;
   }
@@ -66,8 +67,8 @@ void RenderTargets() {
 
 void RenderScore(void) {
   SDL_Surface* text;
-  SDL_Color color = { 255, 255, 255 };
-  char msg[3];
+  SDL_Color color = { 255, 255, 255, 255};
+  char msg[64];
   sprintf(msg,"%i",score);
   
   text = TTF_RenderText_Solid(font, msg, color);
@@ -84,6 +85,15 @@ void CloseAndExit(void) {
   exit(0);
 }
 
+SDL_Rect DrawRect(float x, float y, int w, int h, int r, int g, int b, int a) {
+  SDL_Rect rect = (SDL_Rect){.x = x, .y = y, .w = w, .h = h};
+   
+  SDL_SetRenderDrawColor(renderer, r,g,b,a);
+  SDL_RenderFillRect(renderer, &rect);
+
+  return rect;
+}
+
 void HandleInput(void) {
   SDL_PumpEvents();
   const Uint8* keys = SDL_GetKeyboardState(NULL);
@@ -97,6 +107,67 @@ void HandleInput(void) {
   if (keys[SDL_SCANCODE_D]) {
     pad_x += PAD_SPEED;
   }
+  // TODO: Pause gets too detected. Should be on keydown
+  if (keys[SDL_SCANCODE_SPACE]) {
+    if (!isPaused) {
+      SDL_SetWindowTitle(window, "Brekout (Paused)");
+      isPaused = true;
+    }
+    else {
+      SDL_SetWindowTitle(window, "Brekout");
+      isPaused = false;
+    }
+  }
+}
+
+void update() {
+  if (isPaused) {
+    return;
+  }
+  SDL_SetRenderDrawColor(renderer, 0,0,0, 255);
+  SDL_RenderClear(renderer);
+
+  // Nice ball. Much round
+  SDL_Rect ball = DrawRect(ball_x, ball_y, BALL_SIZE, BALL_SIZE, 0,0,255,255);
+  SDL_Rect pad  = DrawRect(pad_x, pad_y, PAD_WIDTH, PAD_HEIGHT, 90,0,100,255);
+
+  RenderTargets();
+  RenderScore();
+    
+  SDL_RenderPresent(renderer);
+
+  float nx = ball_x + ball_dx*BALL_SPEED*DELTA_TIME;
+  float ny = ball_y + ball_dy*BALL_SPEED*DELTA_TIME;
+  if (nx < 0 || nx + BALL_SIZE > SCREEN_WIDTH) {
+    ball_dx *= -1;
+  } else if (ny < 0 || ny + BALL_SIZE > SCREEN_HEIGHT) {
+    ball_dy *= -1;
+  }
+
+  if (pad_x < 0) {
+    pad_x = 1;
+  } else if (pad_x + PAD_WIDTH > SCREEN_WIDTH) {
+    pad_x = SCREEN_WIDTH - PAD_WIDTH- 1;
+  }
+
+  ball_x = nx;
+  ball_y = ny;
+
+  if (SDL_HasIntersection(&pad, &ball) == SDL_TRUE) {
+    ball_dy *= -1;
+    // Michau problems require Michau solutions
+    ball_y -= 20;
+  }
+  for (int i = 0; i < TARGET_CAP; i++) {
+    Target* t = &targets[i];
+    if (SDL_HasIntersection(&ball, &t->rect) == SDL_TRUE && !t->isDestroyed) {
+      t->isDestroyed = true;
+      score++;
+    }
+  }
+    
+  // Framerate dependent movement KEKW
+  SDL_Delay(1000/FPS);
 }
 
 int main(void) {
@@ -116,64 +187,10 @@ int main(void) {
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   InitTargets();
   
-  bool isPaused = false;
-
-  while (!isPaused) {
-    // Nice ball btw
-    SDL_Rect ball = (SDL_Rect){.x = ball_x, .y = ball_y, .w = BALL_SIZE, .h = BALL_SIZE};
-    SDL_Rect pad = (SDL_Rect){.x = pad_x, .y = pad_y, .w = PAD_WIDTH, .h = PAD_HEIGHT};
-    
-    SDL_SetRenderDrawColor(renderer, 0,0,0, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    SDL_RenderFillRect(renderer, &ball);
-
-    SDL_SetRenderDrawColor(renderer, 90, 0, 100, 255);
-    SDL_RenderFillRect(renderer, &pad);
-
-    RenderTargets();
-    RenderScore();
-    
-    SDL_RenderPresent(renderer);
-
-    float nx = ball_x + ball_dx*BALL_SPEED*DELTA_TIME;
-    float ny = ball_y + ball_dy*BALL_SPEED*DELTA_TIME;
-    if (nx < 0 || nx + BALL_SIZE > SCREEN_WIDTH) {
-      ball_dx *= -1;
-    } else if (ny < 0 || ny + BALL_SIZE > SCREEN_HEIGHT) {
-      ball_dy *= -1;
-    }
-
-    if (pad_x < 0) {
-      pad_x = 1;
-    } else if (pad_x + PAD_WIDTH > SCREEN_WIDTH) {
-      pad_x = SCREEN_WIDTH - PAD_WIDTH- 1;
-    }
-
-    ball_x = nx;
-    ball_y = ny;
-
-    if (SDL_HasIntersection(&pad, &ball) == SDL_TRUE) {
-      ball_dy *= -1;
-      // Michau problems require Michau solutions
-      ball_y -= 20;
-    }
-    for (int i = 0; i < TARGET_CAP; i++) {
-      Target* t = &targets[i];
-	if (SDL_HasIntersection(&ball, &t->rect) == SDL_TRUE) {
-	  t->isDestroyed = true;
-	  // The score system def. working as intended
-	  score++;
-	}
-    }
-    
+  while (1) {
     HandleInput();
-    // Framerate dependent movement KEKW
-    SDL_Delay(1000/FPS);
+    update();
   }
-  
-
   CloseAndExit();
   return 0;
 }
